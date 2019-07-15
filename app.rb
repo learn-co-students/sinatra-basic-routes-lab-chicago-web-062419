ed0fe1781ed2f6cfd0b66e533397a98e14f7672f
Iuliu Padurean <ciprian.padurean@gmail.com>
2019-07-15 10:43:04 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Ciprian!"
+      end
+
+
+  get '/hometown' do
+    "My hometown is Alba Iulia"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Se Preparo - Ozuna!"
+  end
+
 end
+
