117d9402de49df2ed65e6a54fd71c6bc4374f839
Erica Hawkins <ehawk901@gmail.com>
2019-07-15 10:02:22 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do 
+    "My name is Erica"
+  end
+
+  get '/hometown' do 
+    "My hometown is Dallas."
+  end 
+
+  get '/favorite-song' do 
+    "My favorite song is Man in the Mirror."
+  end 
+
 end
