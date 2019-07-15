a4311a691d16d48df60dd7d82fbab0618f57aeae
Philip Roth <50246817+rothberry@users.noreply.github.com>
2019-07-15 10:17:25 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Phil"
+    end
+
+    get '/hometown' do
+        "My hometown is Farmington Hills"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Speakboxx"
+    end
 end
+
